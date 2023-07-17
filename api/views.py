1c914e396db556d515a0b071d7e6c4e2c4d79c6c
santideveloper-lab <contacto.ssnt@gmail.com>
2023-07-17 17:57:16 -0300

    Se agrega el proyecto

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import ProgrammerSerialzier
+from .models import Programmer
+# Create your views here.
+
+
+
+class ProgrammerViewSet(viewsets.ModelViewSet):
+    queryset = Programmer.objects.all()
+    serializer_class = ProgrammerSerialzier
